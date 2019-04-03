0dc4ad1688e9f6ffd57b03df966a2be01d4003d1
Dayvson <dayvson.red@gmail.com>
2019-04-02 21:47:39 -0300

    ini projeto

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FrontChatV3.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrontChatV3.WebApiApplication" Language="C#" %>
